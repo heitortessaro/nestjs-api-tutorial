0af407fb90b0b95cd8c85ef53938c51a38736d17
Heitor <heitor.j.tessaro@gmail.com>
2023-04-23 20:54:09 -0300

    feat: add controller and service to Auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { BookmarkModule } from './bookmark/bookmark.module';
+
 @Module({
   imports: [AuthModule, UserModule, BookmarkModule],
 })
