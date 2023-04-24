fa375833aad316164aee6c8fe6c19a68dcdccfdb
Heitor <heitor.j.tessaro@gmail.com>
2023-04-23 21:00:19 -0300

    feat: add signin and signup function to auth services and controllers

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -4,13 +4,14 @@ import { AuthService } from './auth.service';
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) { }
+
   @Post('signup')
   signup() {
-    return 'I am signed up';
+    return this.authService.signup();
   }
 
   @Post('signin')
   signin() {
-    return 'I am signin in';
+    return this.authService.signin();
   }
 }
