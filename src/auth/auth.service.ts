57dac4ba2f730923d1f8d6720f473e9e5bc240db
Heitor <heitor.j.tessaro@gmail.com>
2023-04-26 06:54:07 -0300

    feat: add an error handler for duplicated user creation
    if an user with the same email exists, an error is thrown

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,31 +1,37 @@
-import { Injectable } from '@nestjs/common';
-import { PrismaService } from 'src/prisma/prisma.service';
+import { ForbiddenException, Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
 import { AuthDto } from './dto';
 import * as argon from 'argon2';
+import { Prisma } from '@prisma/client';
 
 @Injectable({})
 export class AuthService {
   constructor(private prisma: PrismaService) { }
 
   async signup(dto: AuthDto) {
-    //  generate password hash
+    // generate the password hash
     const hash = await argon.hash(dto.password);
     // save the new user in the db
-    const user = await this.prisma.user.create({
-      data: {
-        email: dto.email,
-        hash,
-      },
-      // select: {
-      //   id: true,
-      //   email: true,
-      //   createdAt: true,
-      // },
-    });
+    try {
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hash,
+        },
+      });
 
-    delete user.hash;
-    // return the saved user
-    return user;
+      delete user.hash;
+      return user;
+    } catch (error) {
+      if (error instanceof Prisma.PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          throw new ForbiddenException(
+            'Credentials taken. There is a unique constraint violation',
+          );
+        }
+      }
+      throw error;
+    }
   }
 
   signin() {
