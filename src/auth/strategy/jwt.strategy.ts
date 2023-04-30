213fa8defc48421ca125c3e3c28cf7069fc1a4d4
Heitor <heitor.j.tessaro@gmail.com>
2023-04-30 09:09:49 -0300

    feat: defines a JwtStrategy for validation
    it will be used to protect routes. The token should be provided as a Bearer token in the header.

---
+++ src/auth/strategy/jwt.strategy.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+
+// based on the Implmenting Passport JWT content (https://docs.nestjs.com/recipes/passport#implementing-passport-jwt)
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(config: ConfigService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: config.get('JWT_SECRET'),
+    });
+  }
+}
