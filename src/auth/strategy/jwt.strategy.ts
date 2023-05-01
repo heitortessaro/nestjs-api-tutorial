737d4308a07656d0458981bc7a260fa7f4349add
Heitor <heitor.j.tessaro@gmail.com>
2023-05-01 19:48:52 -0300

    fix: defines the jwt strategy name as jwt and add the validate function
    the validate func is necessary. In this case, the function only returns the received data. However, other validation could be used within this function validate.

--- src/auth/strategy/jwt.strategy.ts
+++ src/auth/strategy/jwt.strategy.ts
@@ -5,11 +5,15 @@ import { ExtractJwt, Strategy } from 'passport-jwt';
 
 // based on the Implmenting Passport JWT content (https://docs.nestjs.com/recipes/passport#implementing-passport-jwt)
 @Injectable()
-export class JwtStrategy extends PassportStrategy(Strategy) {
+export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
   constructor(config: ConfigService) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       secretOrKey: config.get('JWT_SECRET'),
     });
+  }
+
+  validate(payload: any) {
+    return payload;
   }
 }
