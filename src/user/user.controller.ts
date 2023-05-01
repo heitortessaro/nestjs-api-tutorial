00e3396d6c8b351f4b34fbafb1176ad3a25303c8
Heitor <heitor.j.tessaro@gmail.com>
2023-05-01 20:15:20 -0300

    feat: creta an jwt.guard to make the user.controller less verbose
    It does not change how the guard works

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,12 +1,12 @@
 import { Controller, Get, Req, UseGuards } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
 import { Request } from 'express';
+import { JwtGuard } from 'src/auth/guard';
 
 @Controller('users')
 export class UserController {
   // users/me
   // The useGuard use the authGuard, which is implementing the jwt strategy
-  @UseGuards(AuthGuard('jwt'))
+  @UseGuards(JwtGuard)
   @Get('me')
   // the Request comes from Express. It enables to access the request data
   getMe(@Req() req: Request) {
