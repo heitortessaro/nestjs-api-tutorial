99e3ce3f5b73c172365a71a1efbaf61d1aaf2214
Heitor <heitor.j.tessaro@gmail.com>
2023-05-02 19:11:19 -0300

    fix: update files import path

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,7 +1,7 @@
 import { Controller, Get, UseGuards } from '@nestjs/common';
 import { User } from '@prisma/client';
-import { GetUser } from 'src/auth/decorator';
-import { JwtGuard } from 'src/auth/guard';
+import { GetUser } from '../auth/decorator';
+import { JwtGuard } from '../auth/guard';
 
 // The useGuard use the authGuard, which is implementing the jwt strategy
 // The guard protects all routes of the controler
