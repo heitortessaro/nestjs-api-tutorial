35475948e1ffd4094276f858d24275960274c22c
Heitor <heitor.j.tessaro@gmail.com>
2023-05-01 21:55:59 -0300

    refactor: update the user.controller with the JwtGuard and GetUser
    The Guard was moved outside the class to protec all routes from the controller

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,15 +1,18 @@
-import { Controller, Get, Req, UseGuards } from '@nestjs/common';
-import { Request } from 'express';
+import { Controller, Get, UseGuards } from '@nestjs/common';
+import { User } from '@prisma/client';
+import { GetUser } from 'src/auth/decorator';
 import { JwtGuard } from 'src/auth/guard';
 
+// The useGuard use the authGuard, which is implementing the jwt strategy
+// The guard protects all routes of the controler
+@UseGuards(JwtGuard)
 @Controller('users')
 export class UserController {
   // users/me
-  // The useGuard use the authGuard, which is implementing the jwt strategy
-  @UseGuards(JwtGuard)
   @Get('me')
-  // the Request comes from Express. It enables to access the request data
-  getMe(@Req() req: Request) {
-    return req.user;
+  // Use the GetUser decorator defined in auth
+  // user has the User type defined by Prisma
+  getMe(@GetUser() user: User) {
+    return user;
   }
 }
