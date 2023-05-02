2ee4ab0f6a61b1bc33739e3b4975d0f2f2a26948
Heitor <heitor.j.tessaro@gmail.com>
2023-05-02 19:58:02 -0300

    feat: define the list of all test to be developed

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,9 +1,11 @@
 import { Test } from '@nestjs/testing';
 import { AppModule } from '../src/app.module';
 import { INestApplication, ValidationPipe } from '@nestjs/common';
+import { PrismaService } from '../src/prisma/prisma.service';
 
 describe('App e2e', () => {
   let app: INestApplication;
+  let prisma: PrismaService;
   // Build a module for the test
   // uses the AppModule for it
   beforeAll(async () => {
@@ -16,9 +18,34 @@ describe('App e2e', () => {
     app = moduleRef.createNestApplication();
     app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
     await app.init();
+
+    // defines primsa as the prisma service based on the module
+    prisma = app.get(PrismaService);
+    // clean the db before tests
+    await prisma.cleandb();
   });
 
   afterAll(() => {
     app.close();
+  });
+
+  describe('Auth', () => {
+    describe('Signup', () => {
+      it.todo('should signup');
+    });
+    describe('Signin', () => { });
+  });
+
+  describe('User', () => {
+    describe('Get me', () => { });
+    describe('Edit user', () => { });
+  });
+
+  describe('Bookmarks', () => {
+    describe('Create bookmark', () => { });
+    describe('Get bookmark', () => { });
+    describe('Get bookmark by id', () => { });
+    describe('Edit bookmark', () => { });
+    describe('Delete bookmark', () => { });
   });
 });
