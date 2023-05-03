6b6f27587e3b2bc1d8bddec1279e5aadba292135
Heitor <heitor.j.tessaro@gmail.com>
2023-05-03 19:26:04 -0300

    fix: the get-user.decorator need to return the user data when it is provided

--- src/auth/decorator/get-user.decorator.ts
+++ src/auth/decorator/get-user.decorator.ts
@@ -1,11 +1,14 @@
 import { ExecutionContext, createParamDecorator } from '@nestjs/common';
 
 export const GetUser = createParamDecorator(
-  (data: unknown, ctx: ExecutionContext) => {
+  (data: string | undefined, ctx: ExecutionContext) => {
     // make the context change to HTTP and then get the request
     // is is the same of using Express.Request
     // Thus we access the request and get the user info
     const request = ctx.switchToHttp().getRequest();
+    if (data) {
+      return request.user[data];
+    }
     return request.user;
   },
 );
