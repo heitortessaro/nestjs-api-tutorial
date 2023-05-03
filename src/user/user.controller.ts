3ed9d943cb008f4c4086842a4b1a987b73dbd782
Heitor <heitor.j.tessaro@gmail.com>
2023-05-03 19:24:28 -0300

    feat: create the user.service and user.controler functions to update users

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,18 +1,27 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Patch, UseGuards } from '@nestjs/common';
 import { User } from '@prisma/client';
 import { GetUser } from '../auth/decorator';
 import { JwtGuard } from '../auth/guard';
+import { EditUserDto } from './dto';
+import { UserService } from './user.service';
 
 // The useGuard use the authGuard, which is implementing the jwt strategy
 // The guard protects all routes of the controler
 @UseGuards(JwtGuard)
 @Controller('users')
 export class UserController {
+  constructor(private userService: UserService) { }
+
   // users/me
   @Get('me')
   // Use the GetUser decorator defined in auth
   // user has the User type defined by Prisma
   getMe(@GetUser() user: User) {
     return user;
+  }
+
+  @Patch()
+  editUser(@GetUser('id') userId: number, @Body() dto: EditUserDto) {
+    return this.userService.editUser(userId, dto);
   }
 }
